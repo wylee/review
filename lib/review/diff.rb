f5832bea263fba1789d673ab8794173b351c32bb
Wyatt Lee Baldwin <wyatt.lee.baldwin@gmail.com>
2013-10-16 17:29:51 -0700

    Refactor diff hierarchy into sections
    Added a Section class that handles the common logic of finding header
    and content lines and creating subsections. It includes all the logic
    from the Parser class, which was removed, plus it includes the logic
    from File#get_header_lines, which was also removed.
    
    Made Diff, File, and Hunk subclasses of Section. APIs remain almost the
    same. All classes now have #text and #subsections. Hunk no longer has
    #lines.

--- lib/review/diff.rb
+++ lib/review/diff.rb
@@ -1,29 +1,110 @@
 module Review
-  class Diff
-    attr_reader :files
+  class Section
+    @marker = nil
+    @subsection_type = nil
+
+    attr_reader :text
+    attr_reader :subsections
+
+    class << self
+      attr_reader :marker
+      attr_reader :subsection_type
+    end
 
     def initialize(text)
-      lines = text.split("\n")
-      @files = Parser.new(lines, 'diff --git', File).parse
+      @text = text
+      @header_lines = header_lines
+      @content_lines = content_lines
+      @subsections = subsections
+    end
+
+    def header_lines
+      lines = []
+      type = self.class.subsection_type
+      return lines if type.nil?
+      marker = type.marker
+      @text.each_line do |line|
+        break if line.nil? || line.start_with?(marker)
+        lines.push(line)
+      end
+      lines
+    end
+
+    def content_lines
+      @text.lines.drop(@header_lines.length)
+    end
+
+    def subsections
+      subsections = []
+      type = self.class.subsection_type
+      return subsections if type.nil?
+      i = 0
+      lines = @content_lines
+      marker = type.marker
+      until i == lines.length
+        line = lines[i]
+        if line.start_with?(marker)
+          j = i + 1
+          line = lines[j]
+          until line.nil? || line.start_with?(marker)
+            j += 1
+            line = lines[j]
+          end
+          text = lines[i...j].join()
+          subsection = type.new(text)
+          subsections.push(subsection)
+          i = j
+        end
+      end
+      subsections
     end
   end
 
-  class File
+  # TODO: Detect changed lines
+  # TODO: Parse @@ line
+  class Hunk < Section
+    @marker = '@@'
+
+    attr_reader :added_lines
+    attr_reader :removed_lines
+
+    def initialize(text)
+      super(text)
+
+      @added_lines = []
+      @removed_lines = []
+
+      @content_lines.each do |line|
+        first_char = line[0]
+        if first_char == '+' || first_char == '-'
+          line = line[1..-1].chomp
+          if first_char == '+'
+            @added_lines.push(line)
+          else
+            @removed_lines.push(line)
+          end
+        end
+      end
+    end
+  end
+
+  class File < Section
+    @marker = 'diff --git'
+    @subsection_type = Hunk
+
     attr_reader :created
-    attr_reader :hunks
     attr_reader :name
     attr_reader :original_name
     attr_reader :removed
     attr_reader :renamed
 
-    def initialize(lines)
-      header_lines = get_header_lines(lines)
-      hunk_lines = lines[header_lines.length..-1]
+    alias_method :hunks, :subsections
 
-      @hunks = Parser.new(hunk_lines, '@@', Hunk).parse
+    def initialize(text)
+      super(text)
 
       original_name, new_name = nil, nil
-      header_lines.each do |line|
+      @header_lines.each do |line|
         if line.start_with?('---')
           original_name = line[4..-1].chomp
         elsif line.start_with?('+++')
@@ -57,73 +138,18 @@ module Review
       @original_name = original_name
     end
 
-    def get_header_lines(lines)
-      headers = []
-      lines.each do |line|
-        break if line.nil? || line.start_with?('@@')
-        headers.push(line)
-      end
-      headers
-    end
-
     # Return all changed lines of all hunks
     def lines
       all_lines = []
-      @hunks.each {|h| all_lines.concat(h.lines) }
+      @subsections.each do |h|
+        all_lines += h.added_lines + h.removed_lines
+      end
       all_lines
     end
   end
 
-  # TODO: Detect changed lines
-  # TODO: Parse @@ line
-  class Hunk
-    attr_reader :lines
-    attr_reader :added_lines
-    attr_reader :removed_lines
-
-    def initialize(lines)
-      @lines = []
-      @added_lines = []
-      @removed_lines = []
-      lines.each do |line|
-        first_char = line[0]
-        if first_char == '+' || first_char == '-'
-          line = line[1..-1].chomp
-          @lines.push(line)
-          if first_char == '+'
-            @added_lines.push(line)
-          else
-            @removed_lines.push(line)
-          end
-        end
-      end
-    end
-  end
-
-  class Parser
-    def initialize(lines, marker, type)
-      @lines = lines
-      @marker = marker
-      @type = type
-    end
-
-    def parse
-      i = 0
-      items = []
-      until i == @lines.length
-        line = @lines[i]
-        if line.start_with?(@marker)
-          j = i + 1
-          line = @lines[j]
-          until line.nil? || line.start_with?(@marker)
-            j += 1
-            line = @lines[j]
-          end
-          items.push(@type.new(@lines[i...j]))
-          i = j
-        end
-      end
-      items
-    end
+  class Diff < Section
+    @subsection_type = File
+    alias_method :files, :subsections
   end
 end
